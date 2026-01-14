39ec7aef948f039dc0648573bae2f533818591a2
aaltatan <a.altatan@gmail.com>
2026-01-14 07:50:55 +0300

    refactor routers

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,10 +2,16 @@ from fastapi import APIRouter
 
 from . import tenant_superusers, tenants, users
 
+# v1 main router
 router = APIRouter()
-
 router.include_router(tenants.router, prefix="/tenants", tags=["tenants"])
-router.include_router(
-    tenant_superusers.router, prefix="/{tenant_code}/tenant-superusers", tags=["Tenant Superusers"]
+
+# v1 tenant router
+tenant_router = APIRouter()
+tenant_router.include_router(
+    tenant_superusers.router, prefix="/tenant-superusers", tags=["Tenant Superusers"]
 )
-router.include_router(users.router, prefix="/{tenant_code}/users", tags=["users"])
+tenant_router.include_router(users.router, prefix="/users", tags=["users"])
+
+# v1 router
+router.include_router(tenant_router, prefix="/{tenant_code}")
