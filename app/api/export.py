fd60fe207288133628d94d1a0ba8ecfb04784677
aaltatan <a.altatan@gmail.com>
2026-02-20 04:58:46 +0300

    refactoring

--- app/api/export.py
+++ app/api/export.py
@@ -10,11 +10,6 @@ from fastapi.responses import StreamingResponse
 from app.core.timezone import get_default_tz_now
 
 
-class ExportType(StrEnum):
-    CSV = auto()
-    XLSX = auto()
-
-
 class IExportSchema(Protocol):
     @classmethod
     def model_validate(cls, *args: Any, **kwargs: Any) -> Self: ...
@@ -25,44 +20,56 @@ class ISaveable(Protocol):
     def save(self, *args: Any, **kwargs: Any) -> None: ...
 
 
-type ExportFn = Callable[[Sequence[IExportSchema], str], tuple[ISaveable, str]]
+type Data = Sequence[IExportSchema]
+type ExportFn = Callable[[Data, str], tuple[ISaveable, str]]
 
 
-_export_fns: dict[ExportType, tuple[ExportFn, str]] = {}
+class ExportFormat(StrEnum):
+    CSV = auto()
+    XLSX = auto()
 
 
-def get_export_args(export_type: ExportType) -> tuple[ExportFn, str]:
-    if export_type not in _export_fns:
-        message = f"no export function registered for {export_type}"
-        raise ValueError(message)
-
-    return _export_fns[export_type]
+class ExporterFnNotFoundError(Exception):
+    def __init__(self, fmt: ExportFormat, *args: object) -> None:
+        message = f"No export function registered for {fmt.value}"
+        super().__init__(message, *args)
 
 
-def register_exporter(export_type: ExportType, media_type: str) -> Callable[[ExportFn], ExportFn]:
-    def decorator(fn: ExportFn) -> ExportFn:
-        @wraps(fn)
-        def wrapper(schema_list: Sequence[IExportSchema], suffix: str) -> Any:
-            now = get_default_tz_now().strftime("%Y-%m-%d_%H-%M-%S")
-            filename = f"{suffix}_{now}.{export_type.value}"
-            return fn(schema_list, filename)
+class ExporterRegistry:
+    def __init__(self) -> None:
+        self._exporters: dict[ExportFormat, tuple[ExportFn, str]] = {}
 
-        _export_fns[export_type] = (wrapper, media_type)
+    def __getitem__(self, fmt: ExportFormat) -> tuple[ExportFn, str]:
+        if fmt not in self._exporters:
+            raise ExporterFnNotFoundError(fmt)
 
-        return wrapper
+        return self._exporters[fmt]
 
-    return decorator
+    def register(self, fmt: ExportFormat, media_type: str) -> Callable[[ExportFn], ExportFn]:
+        def decorator(fn: ExportFn) -> ExportFn:
+            @wraps(fn)
+            def wrapper(schema_list: Data, suffix: str) -> Any:
+                now = get_default_tz_now().strftime("%Y-%m-%d_%H-%M-%S")
+                filename = f"{suffix}_{now}.{fmt.value}"
+                return fn(schema_list, filename)
+
+            self._exporters[fmt] = (wrapper, media_type)
+
+            return wrapper
+
+        return decorator
 
 
-@register_exporter(
-    ExportType.XLSX, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+exporters = ExporterRegistry()
+
+
+@exporters.register(
+    ExportFormat.XLSX, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 )
-def export_to_xlsx(
-    schema_list: Sequence[IExportSchema], suffix: str
-) -> tuple[openpyxl.Workbook, str]:
+def export_to_xlsx(data: Data, suffix: str) -> tuple[openpyxl.Workbook, str]:
     wb = openpyxl.Workbook()
 
-    if not schema_list:
+    if not data:
         return wb, suffix
 
     ws = wb.active
@@ -71,18 +78,18 @@ def export_to_xlsx(
         message = "no active worksheet"
         raise ValueError(message)
 
-    ws.append(list(schema_list[0].model_dump().keys()))
+    ws.append(list(data[0].model_dump().keys()))
 
-    for schema in schema_list:
+    for schema in data:
         ws.append(list(schema.model_dump().values()))
 
     return wb, suffix
 
 
 def get_export_response[T](
-    export: ExportType, data: Sequence[T], schema: IExportSchema, suffix: str
+    fmt: ExportFormat, data: Sequence[T], schema: IExportSchema, suffix: str
 ) -> StreamingResponse:
-    export_fn, media_type = get_export_args(export)
+    export_fn, media_type = exporters[fmt]
 
     schema_list = [schema.model_validate(obj) for obj in data]
 
